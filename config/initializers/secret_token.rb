2c2de86faa33246527647c0e4f1a3efc57e11bc5
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2013-09-10 13:30:38 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EdgarBlogs::Application.config.secret_key_base = '91a907bb631875641090459cd69db2a19471b8a9625053f2aff975de767c78441bf5f05b0e52211861d461154f0c323cb88e8707d73b4cbda545a6474119a29f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EdgarBlogs::Application.config.session_store :cookie_store, key: '_edgar_blogs_session'
