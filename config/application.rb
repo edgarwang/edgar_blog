80179c29f6593a47e24180464ddff3de30a62345
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2013-09-13 13:13:34 +0800

    disable helper spec

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module EdgarBlogs
       g.test_framework :rspec,
         fixtures: true,
         view_specs: false,
-        helper_specs: true,
+        helper_specs: false,
         routing_specs: false,
         controller_specs: true,
         request_specs: false
