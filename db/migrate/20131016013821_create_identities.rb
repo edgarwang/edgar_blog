7fde711f54b97c3d3b36751cdcd7ee4a2ad14fa5
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2013-10-16 16:47:51 +0800

    Add User model

--- db/migrate/20131016013821_create_identities.rb
+++ db/migrate/20131016013821_create_identities.rb
@@ -1,9 +1,11 @@
 class CreateIdentities < ActiveRecord::Migration
   def change
     create_table :identities do |t|
-      t.string :name
       t.string :email
       t.string :password_digest
+      t.string :uid
+      t.string :provider
+      t.references :user
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131016013821) do
+ActiveRecord::Schema.define(version: 20131016084729) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -23,9 +23,17 @@ ActiveRecord::Schema.define(version: 20131016013821) do
   end
 
   create_table "identities", force: true do |t|
-    t.string   "name"
     t.string   "email"
     t.string   "password_digest"
+    t.string   "uid"
+    t.string   "provider"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
