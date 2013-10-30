6080ee7fa2ee567263579bfc6dd65567eade382a
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2013-10-30 22:30:24 +0800

    tweak puma conf file

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,8 @@
 threads 4,32
 workers 2
-bind "unix://APP_ROOT/tmp/socks/puma.sock"
+bind "unix://RAILS_ROOT/tmp/socks/puma.sock"
 
-pidfile "APP_ROOT/tmp/pids/puma.pid"
-state_path "APP_ROOT/tmp/pids/puma.state"
+pidfile "RAILS_ROOT/tmp/pids/puma.pid"
+state_path "RAILS_ROOT/tmp/pids/puma.state"
 daemonize true 
 preload_app!
