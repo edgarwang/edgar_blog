3915c8458edb3c02d1b64146a8b20e420259571b
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2013-11-11 22:05:58 +0800

    add a article_edit_tag helper

--- app/helpers/dashboard/articles_helper.rb
+++ app/helpers/dashboard/articles_helper.rb
@@ -14,4 +14,16 @@ module Dashboard::ArticlesHelper
   def not_published
     not published?
   end
+
+  def article_edit_tag(article, id)
+    article_info = {
+      id: article.id,
+      slug: article.slug,
+      status: article.status
+    }
+    raw text_area_tag('article[content]',
+                  article.content,
+                  id: id,
+                  data: article_info)
+  end
 end
